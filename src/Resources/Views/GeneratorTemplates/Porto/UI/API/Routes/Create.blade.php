0a29046b6be8446878ea0c81160bac2b8b5ff70c
Johan Alvarez <llstarscreamll@hotmail.com>
2017-02-12 02:33:35 -0500

    setup relations for Porto

--- src/Resources/Views/GeneratorTemplates/Porto/UI/API/Routes/Create.blade.php
+++ src/Resources/Views/GeneratorTemplates/Porto/UI/API/Routes/Create.blade.php
@@ -1,1 +1,8 @@
 <?= "<?php\n" ?>
+
+$router->post('{{ str_slug($gen->tableName, $separator = "_") }}/create', [
+    'uses'  => 'Controller@createAdmin',
+    'middleware' => [
+      'api.auth',
+    ],
+]);
