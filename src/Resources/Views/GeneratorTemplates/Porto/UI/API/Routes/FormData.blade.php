ddd1c3bc712e0fdfa0002e68f8dcffc44213c65a
Johan Alvarez <llstarscreamll@hotmail.com>
2017-03-07 06:24:11 +0000

    renaming form data endpoint

--- src/Resources/Views/GeneratorTemplates/Porto/UI/API/Routes/FormData.blade.php
+++ src/Resources/Views/GeneratorTemplates/Porto/UI/API/Routes/FormData.blade.php
@@ -1,6 +1,6 @@
 <?= "<?php\n" ?>
 
-$router->get('{{ str_slug($gen->tableName, $separator = "-") }}/{{ $gen->slugEntityName() }}-form-data', [
+$router->get('{{ str_slug($gen->tableName, $separator = "-") }}/form-data/{{ $gen->slugEntityName() }}', [
     'uses'  => 'Controller{{ "@".camel_case($gen->entityName()) }}formData',
     'middleware' => [
       'api.auth',
