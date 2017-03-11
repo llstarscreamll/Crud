6892c6e58ed6e6aa475f657f471ff841c063d970
Johan Alvarez <llstarscreamll@hotmail.com>
2017-03-10 19:55:56 -0500

    changin translations folder

---
+++ Resources/Languages/es/templates.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+	'create-link' => 'Crear',
+	'details-link' => 'Detalles',
+	'edit-link' => 'Editar',
+	'delete-link' => 'Borrar',
+	'restore-link' => 'Restaurar',
+	'yes' => 'Si',
+	'no' => 'No',
+	'confirm_field_prefix' => 'Confirmar',
+	'create_success' => 'El :item ha sido creado correctamente',
+];
