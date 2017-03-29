d388f8091739d4de9ef8a45fd40c0e288c38957b
Johan Alvarez <llstarscreamll@hotmail.com>
2017-03-29 23:40:01 +0000

    add Ng2BootstrapModule to generated module [Angular]

--- UI/WEB/Views/GeneratorTemplates/Angular2/module.blade.php
+++ UI/WEB/Views/GeneratorTemplates/Angular2/module.blade.php
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
+
 import { {{ $gen->moduleClass('routing') }} } from './{{ str_replace('.ts', '', $gen->moduleFile('routing')) }}';
 // ng2 Translate
 import { TranslateService, TranslateModule } from 'ng2-translate';
@@ -28,6 +30,7 @@ import { SERVICES } from './services';
     ENV.THEME,
     {{ $gen->moduleClass('routing') }},
     ...EFFECTS,
+    Ng2BootstrapModule.forRoot(),
   ],
   declarations: [
     ...COMPONENTS,
