8637c07f2f84ae4feafa22d0f24b1a6c3af706c5
Johan Alvarez <llstarscreamll@hotmail.com>
2017-04-12 19:21:11 +0000

    add function to setup mockBackend for tests [Angular]

--- UI/WEB/Views/GeneratorTemplates/Angular2/utils/:entity:-testing.blade.php
+++ UI/WEB/Views/GeneratorTemplates/Angular2/utils/:entity:-testing.blade.php
@@ -1,1 +1,14 @@
+import { MockBackend, MockConnection } from '@angular/http/testing';
+import { Http, HttpModule, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
+
 export const FORM_MODEL = {!! json_encode($gen->getFormModelConfigArray($fields)) !!};
+
+export function setupMockBackend(mockBackend: MockBackend) {
+	mockBackend.connections.subscribe((connection: MockConnection) => {
+	  if (connection.request.url.includes('form-model')) {
+	    connection.mockRespond(new Response(new ResponseOptions({
+	      body: JSON.stringify(FORM_MODEL)
+	    })));
+	  }
+	});
+}
