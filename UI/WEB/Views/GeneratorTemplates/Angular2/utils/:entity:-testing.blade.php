cf020265f06f3c827c98803314e6aa200ed761f5
Johan Alvarez <llstarscreamll@hotmail.com>
2017-04-13 00:29:26 +0000

    add testing models [Angular]

--- UI/WEB/Views/GeneratorTemplates/Angular2/utils/:entity:-testing.blade.php
+++ UI/WEB/Views/GeneratorTemplates/Angular2/utils/:entity:-testing.blade.php
@@ -1,6 +1,8 @@
 import { MockBackend, MockConnection } from '@angular/http/testing';
 import { Http, HttpModule, BaseRequestOptions, Response, ResponseOptions } from '@angular/http';
 
+import { {{ $model = $gen->entityName() }} } from './../models/{{ camel_case($gen->entityName()) }}';
+
 export const FORM_MODEL = {!! json_encode($gen->getFormModelConfigArray($fields)) !!};
 
 export let translateKey: string = '{{ $gen->entityNameSnakeCase() }}.';
@@ -10,6 +12,13 @@ export let tableColumns = [
   '{{ $gen->tableName.'.'.$field->name }}',
 @endif
 @endforeach
+];
+
+export let {{ $gen->entityName() }}One: {{ $model }} = {!! json_encode(['id' => 'a1'] + factory('App\Containers\\'.$gen->containerName().'\\Models\\'.$gen->entityName())->make()->toArray()) !!};
+export let {{ $gen->entityName() }}Two: {{ $model }} = {!! json_encode(['id' => 'b2'] + factory('App\Containers\\'.$gen->containerName().'\\Models\\'.$gen->entityName())->make()->toArray()) !!};
+export let {{ $gen->entityName() }}List: {{ $model }}[] = [
+	{{ $gen->entityName() }}One,
+	{{ $gen->entityName() }}Two,
 ];
 
 export function setupMockBackend(mockBackend: MockBackend) {
