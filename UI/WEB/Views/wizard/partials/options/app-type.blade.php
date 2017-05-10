e135107c6ff2db072145b43e4330dcc8bd8f7bf4
Johan Alvarez <llstarscreamll@hotmail.com>
2017-05-10 05:29:24 +0000

    some improvements on options setup view

--- UI/WEB/Views/wizard/partials/options/app-type.blade.php
+++ UI/WEB/Views/wizard/partials/options/app-type.blade.php
@@ -1,16 +1,22 @@
 <fieldset>
 	
 	<legend>App Type</legend>
-
-	<div class="form-group col-sm-6 col-md-2">
-        <label>
-            {!! Form::checkbox('app_type[]', 'porto_container', true, ['class' => 'icheckbox_square-blue']) !!}
-            PORTO Container
-        </label>
-        <label>
-            {!! Form::checkbox('app_type[]', 'angular_2_module', true, ['class' => 'icheckbox_square-blue']) !!}
-            Angular 2 Module
-        </label>
+    <div class="row">
+        <div class="form-group col-sm-6 col-md-4">
+            <div class="radio">
+                <label>
+                    {!! Form::checkbox('app_type[]', 'porto_container', true, ['class' => 'icheckbox_square-blue']) !!}
+                    PORTO Container
+                </label>
+            </div>
+            <div class="radio">
+                <label>
+                    {!! Form::checkbox('app_type[]', 'angular_2_module', true, ['class' => 'icheckbox_square-blue']) !!}
+                    Angular Module
+                </label>
+            </div>
+            <span class="help-block">For <strong>Angular module</strong> you must have your generated PORTO container on the <code>{{ app_path('Containers') }}</code> folder</span>
+        </div>
     </div>
 
 </fieldset>
--- UI/WEB/Views/wizard/partials/options/entity-attributes.blade.php
+++ UI/WEB/Views/wizard/partials/options/entity-attributes.blade.php
@@ -5,16 +5,19 @@
         <div class="form-group col-sm-4 col-md-3">
             {!! Form::label('plural_entity_name', 'Plural Name') !!}
             {!! Form::text('plural_entity_name', null, ['class' => 'form-control']) !!}
+            <span class="help-block">Your entity plural name (Books, User Addresses)</span>
         </div>
 
         <div class="form-group col-sm-4 col-md-3">
             {!! Form::label('single_entity_name', 'Singular Name') !!}
             {!! Form::text('single_entity_name', null, ['class' => 'form-control']) !!}
+            <span class="help-block">Your entity singular name (Book, User Address)</span>
         </div>
 
         <div class="form-group col-sm-4 col-md-2">
             {!! Form::label('is_part_of_package', 'Container/Module') !!}
             {!! Form::text('is_part_of_package', null, ['class' => 'form-control']) !!}
+            <span class="help-block">Should be singular (Library, User)</span>
         </div>
 
         <div class="form-group col-sm-4 col-md-2">
@@ -25,11 +28,13 @@
                 isset($names_list['name']) ? $names_list['name'] : null,
                 ['class' => 'form-control']
             ) !!}
+            <span class="help-block">What's the human readable attr?</span>
         </div>
 
         <div class="form-group col-sm-4 col-md-2">
             <label for="language_key">Languaje key</label>
             {!! Form::text('language_key', null, ['class' => 'form-control', 'placeholder' => 'en']) !!}
+            <span class="help-block">Language to generate (es, en, ru)</span>
         </div>
 
         <div class="clearfix"></div>
@@ -95,17 +100,14 @@
                             {!! Form::select("field[$field][relation]",
                                 [
                                 '' => '---',
-                                'hasOne' => 'hasOne',
                                 'belongsTo' => 'belongsTo',
-                                'hasMany' => 'hasMany',
-                                'belongsToMany' => 'belongsToMany',
                                 ],
                                 null,
                                 ['class' => 'form-control input-xs']
                             ) !!}
                         </td>
                         <td>
-                            {!! Form::checkbox("field[$field][fillable]", true, null, [
+                            {!! Form::checkbox("field[$field][fillable]", true, !empty($options) ? null : (in_array($field, ['id', 'created_at', 'updated_at', 'deleted_at']) ? false : true), [
                                 'class' => 'bootstrap_switch',
                                 'data-size' => 'mini',
                                 'data-on-text' => 'Yes',
@@ -121,7 +123,7 @@
                             ]) !!}
                         </td>
                         <td>
-                            {!! Form::checkbox("field[$field][on_index_table]", true, null, [
+                            {!! Form::checkbox("field[$field][on_index_table]", true, !empty($options) ? null : (in_array($field, ['id', 'created_at', 'updated_at', 'deleted_at']) ? false : true), [
                                 'class' => 'bootstrap_switch',
                                 'data-size' => 'mini',
                                 'data-on-text' => 'Yes',
@@ -129,7 +131,7 @@
                             ]) !!}
                         </td>
                         <td>
-                            {!! Form::checkbox("field[$field][on_create_form]", true, null, [
+                            {!! Form::checkbox("field[$field][on_create_form]", true, !empty($options) ? null : (in_array($field, ['id', 'created_at', 'updated_at', 'deleted_at']) ? false : true), [
                                 'class' => 'bootstrap_switch',
                                 'data-size' => 'mini',
                                 'data-on-text' => 'Yes',
@@ -137,7 +139,7 @@
                             ]) !!}
                         </td>
                         <td>
-                            {!! Form::checkbox("field[$field][on_update_form]", true, null, [
+                            {!! Form::checkbox("field[$field][on_update_form]", true, !empty($options) ? null : (in_array($field, ['id', 'created_at', 'updated_at', 'deleted_at']) ? false : true), [
                                 'class' => 'bootstrap_switch',
                                 'data-size' => 'mini',
                                 'data-on-text' => 'Yes',
@@ -145,10 +147,10 @@
                             ]) !!}
                         </td>
                         <td>
-                            {!! Form::text("field[$field][label]", null, ['class' => 'form-control input-xs', 'required', 'placeholder' => $field.' label']) !!}
+                            {!! Form::text("field[$field][label]", (($trans = trans('validation.attributes.'.$field)) != "validation.attributes.$field") ? ucfirst($trans) : null, ['class' => 'form-control input-xs', 'required', 'placeholder' => $field.' label']) !!}
                         </td>
                         <td>
-                            {!! Form::text("field[$field][validation_rules]", null, ['class' => 'form-control input-xs', 'placeholder' => $field.' rules']) !!}
+                            {!! Form::text("field[$field][validation_rules]", in_array($fieldData->type, ['timestamp', 'datetime']) ? 'date:Y-m-d H:i:s' : null, ['class' => 'form-control input-xs', 'placeholder' => $field.' rules']) !!}
                         </td>
                     </tr>
                     @endforeach
