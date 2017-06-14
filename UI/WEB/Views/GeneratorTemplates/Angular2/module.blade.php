75b74f056c51b8609157f719521615160123db53
Johan Alvarez <llstarscreamll@hotmail.com>
2017-06-14 04:17:53 +0000

    [Angular] clean module imports

--- UI/WEB/Views/GeneratorTemplates/Angular2/module.blade.php
+++ UI/WEB/Views/GeneratorTemplates/Angular2/module.blade.php
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { Ng2BootstrapModule } from 'ngx-bootstrap';
-import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { TranslateService } from '@ngx-translate/core';
 
-import { DynamicFormModule } from './../dynamic-form/dynamic-form.module';
 import { CoreSharedModule } from './../core/core.shared.module';
 import { environment } from './../../environments/environment';
+
 import { PAGES } from './pages';
 import { COMPONENTS } from './components';
-import { {{ $gen->getLanguageKey(true) }} } from './translations/{{ $gen->getLanguageKey() }}';
 import { EFFECTS } from './effects';
 import { SERVICES } from './services';
+import { {{ $gen->getLanguageKey(true) }} } from './translations/{{ $gen->getLanguageKey() }}';
 import { {{ $gen->moduleClass('routing') }} } from './{{ str_replace('.ts', '', $gen->moduleFile('routing')) }}';
 
 /**
@@ -21,12 +18,7 @@ import { {{ $gen->moduleClass('routing') }} } from './{{ str_replace('.ts', '', 
  */
 @NgModule({
   imports: [
-    CommonModule,
-    ReactiveFormsModule,
     environment.theme,
-    Ng2BootstrapModule.forRoot(),
-    TranslateModule,
-    DynamicFormModule,
     CoreSharedModule,
     {{ $gen->moduleClass('routing') }},
     ...EFFECTS,
